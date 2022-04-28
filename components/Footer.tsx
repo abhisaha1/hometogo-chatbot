d67c4e43a97e9934fc665dc6f0004151f7d2ad41
Sahil Kathiri <sahil.kathiri@hometogo.com>
2022-04-28 12:21:32 +0300

    Add tailwind and AppShell

---
+++ components/Footer.tsx
@@ -0,0 +1,5 @@
+import { Footer as MantineFooter } from "@mantine/core"
+
+export const Footer = () => {
+  return <MantineFooter height={60} className="border-none">Footer</MantineFooter>
+}
---
+++ components/Header.tsx
@@ -0,0 +1,5 @@
+import { Header as MantineHeader } from "@mantine/core"
+
+export const Header = () => {
+  return <MantineHeader height={60} p="xs" className="border-none">Header</MantineHeader>
+}
